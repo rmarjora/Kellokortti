43d139f49a7da64f728a1383eaf866d5dfffa5a0
rmarjora <rasmus.marjoranta@gmail.com>
2025-08-13 11:56:30 +0300

    initial commit

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    // Development: React dev server
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+  } else {
+    // Production: Load built React files
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Hello from Electron + React + Vite 👋</h1>
+    </div>
+  );
+}
